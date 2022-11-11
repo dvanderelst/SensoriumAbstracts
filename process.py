14681abcb9948ffaa6f2d389d228c96d63a9a058
Lenovo <dieter.vanderelst@fastmail.net>
2022-11-11 10:59:39 -0500

    ertert

--- process.py
+++ process.py
@@ -3,6 +3,12 @@ import pandas
 data = pandas.read_excel('Sensorium_use_4_talks.xlsx', sheet_name='talk schedule', converters={'time': str})
 n = data.shape[0]
 
+f = open('snippet.txt')
+template = f.read()
+f.close()
+
+output = open('output.md', 'w')
+
 for i in range(n):
     talk = data.iloc[i, :]
     time = talk.time
@@ -11,4 +17,16 @@ for i in range(n):
     authors = talk['author list']
     abstract = talk.abstract
     title = talk.title
+
+    filled = template.replace('[time]', time)
+    filled = filled.replace('[presenter]', presenter)
+    filled = filled.replace('[title]', title)
+
+    filled = filled.replace('[email]', email)
+    filled = filled.replace('[authors]', authors)
+    filled = filled.replace('[abstract]', abstract)
+
+    output.write(filled + '\n')
+
+output.close()
 
